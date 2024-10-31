a487be67e9046f195640da173794b97de97765ad
Komo <71205197+mbekkomo@users.noreply.github.com>
2024-10-31 17:58:23 +0800

    changes from goatputer on Thu Oct 31 05:58:23 PM WITA 2024

--- configs/hx-langs.nix
+++ configs/hx-langs.nix
@@ -4,4 +4,17 @@ _: {
       nil.formatting.command = [ "nixfmt" ];
     };
   };
+
+  language-server.emmet-langserver = {
+    command = "emmet-language-server";
+    args = ["--stdio"];
+  };
+
+  language = [
+    {
+      name = "html";
+      roots = [".git"];
+      language-servers = ["emmet-langserver"];
+    }
+  ];
 }
