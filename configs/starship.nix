b146e671984aec6a60a7e41c09a8320feb50faf2
Komo <71205197+mbekkomo@users.noreply.github.com>
2024-10-31 10:48:20 +0800

    changes from goatputer on Thu Oct 31 10:48:20 AM WITA 2024

--- configs/starship.nix
+++ configs/starship.nix
@@ -1,4 +1,11 @@
 _: {
-  format = "> ";
-  right_format = "<";
+  username = {
+    style_root = "bold fg:#ff8533";
+    style_user = "bold fg:#ffff4d";
+    format = "[$user]($style)";
+  };
+
+  format = ''
+  $username
+  >'';
 }
