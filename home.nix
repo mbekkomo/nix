53248021b05ea080ca55e47b881130ebe061d85d
Komo <71205197+mbekkomo@users.noreply.github.com>
2024-11-09 05:37:42 +0800

    changes from goats-laptop on Sat Nov 9 05:37:42 AM WITA 2024

--- home.nix
+++ home.nix
@@ -48,7 +48,7 @@ in
   home.file = {
     ".config/alacritty/themes/tokyonight.toml".source = ./configs/alacritty/tokyonight.toml;
     ".local/share/blackbox/schemes/tokyonight.json".source = ./etc/blackbox/tokyonight.json;
-    ".fonts/departuremono-nerdfont.otf".source = ./fonts/DepartureMonoNerdFont-Regular.otf;
+    ".local/share/fonts/departuremono-nerdfont.otf".source = ./fonts/DepartureMonoNerdFont-Regular.otf;
     ".config/zls.json".text = builtins.toJSON (import ./configs/zls.nix { });
 
     ".config/fish/lscolors.fish".source = ./etc/lscolors.fish;
