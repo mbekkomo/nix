95af02938fe72a3c1cb7886367e82e7a78047599
Komo <71205197+mbekkomo@users.noreply.github.com>
2024-11-09 08:13:12 +0800

    changes from goats-laptop on Sat Nov 9 08:13:12 AM WITA 2024

--- Bakefile.sh
+++ Bakefile.sh
@@ -1,3 +1,7 @@
 task.checkout() {
-  git checkout
+  local name
+  name="$(sed 's| |-|g' /sys/devices/virtual/dmi/id/product_name)" || \
+    name="$(adb shell getprop ro.product.vendor.model)" || \
+    bake.die "unknown machine, you're probably running the rich's os (macosx)"
+  git checkout "${name}" || bake.die "couldn't find the config for your machine's model"
 }
