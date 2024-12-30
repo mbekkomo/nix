fa463d4c2edd7d1c36f02a12e54111ea9ef9b6dc
Komo <afiqquraisyzulkarnain@gmail.com>
2024-12-30 18:29:53 +0800

    changes from nixos on Mon Dec 30 06:29:53 PM WITA 2024

--- configs/hypr/wm.nix
+++ configs/hypr/wm.nix
@@ -29,9 +29,6 @@ in
 
     border_size = 2;
 
-    "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-    "col.inactive_border" = "rgba(595959aa)";
-
     resize_on_border = true;
 
     allow_tearing = false;
