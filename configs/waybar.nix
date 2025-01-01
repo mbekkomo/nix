722c40baee5006475b32a43486b6496cb99d3cb8
Komo <afiqquraisyzulkarnain@gmail.com>
2025-01-02 03:42:10 +0800

    changes from nixos on Thu Jan 2 03:42:10 AM WITA 2025

--- configs/waybar.nix
+++ configs/waybar.nix
@@ -5,33 +5,35 @@
     height = 20;
     spacing = 4;
   
+    # modules-left = [
+    #   "hyprland/workspaces"
+    #   "hyprland/submap"
+    # ];
+
+    # modules-center = [
+    #   "hyprland/window"
+    # ];
+
+    # modules-right = [
+    #   "mpd"
+    #   "idle_inhibitor"
+    #   "pulseaudio"
+    #   "network"
+    #   "power-profiles-daemon"
+    #   "cpu"
+    #   "memory"
+    #   "temperature"
+    #   "backlight"
+    #   "keyboard-state"
+    #   "hyprland/language"
+    #   "battery"
+    #   "battery#bat2"
+    #   "clock"
+    #   "tray"
+    # ];
+
     modules-left = [
-      "hyprland/workspaces"
-      "hyprland/submap"
-    ];
-
-    modules-center = [
-      "hyprland/window"
-    ];
-
-    modules-right = [
-      "mpd"
-      "idle_inhibitor"
-      "pulseaudio"
-      "network"
-      "power-profiles-daemon"
-      "cpu"
-      "memory"
-      "temperature"
-      "backlight"
-      "keyboard-state"
-      "hyprland/language"
-      "battery"
-      "battery#bat2"
       "clock"
-      "tray"
     ];
-
-    
   };
 }
