0978ce4003deda7f7fe9a437123fadfbc724fd16
Komo <afiqquraisyzulkarnain@gmail.com>
2025-01-01 09:33:55 +0800

    changes from nixos on Wed Jan 1 09:33:54 AM WITA 2025

--- Bakefile.sh
+++ Bakefile.sh
@@ -1,7 +1,7 @@
 task.checkout() {
   local name
   name="$(sed 's| |-|g' /sys/devices/virtual/dmi/id/product_name)" || \
-    name="$(adb shell getprop ro.product.vendor.model)" || \
+    name="$(getprop ro.product.vendor.model)" || \
     bake.die "unknown machine, you're probably running the rich's os (macosx)"
   git checkout "${name}" || bake.die "couldn't find the config for your machine's model"
 }
