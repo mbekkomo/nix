453b3f71006bbdfc09ea178ffd8e035d654465fa
Komo <afiqquraisyzulkarnain@gmail.com>
2025-01-02 23:21:04 +0800

    changes from nixos on Thu Jan 2 11:21:04 PM WITA 2025

--- configs/waybar.nix
+++ configs/waybar.nix
@@ -11,45 +11,45 @@ in
     height = 20;
     spacing = 4;
   
-    # modules-left = [
-    #   "hyprland/workspaces"
-    #   "hyprland/submap"
-    # ];
-
-    # modules-center = [
-    #   "hyprland/window"
-    # ];
-
-    # modules-right = [
-    #   "mpd"
-    #   "idle_inhibitor"
-    #   "pulseaudio"
-    #   "network"
-    #   "power-profiles-daemon"
-    #   "cpu"
-    #   "memory"
-    #   "temperature"
-    #   "backlight"
-    #   "keyboard-state"
-    #   "hyprland/language"
-    #   "battery"
-    #   "battery#bat2"
-    #   "clock"
-    #   "tray"
-    # ];
-
     modules-left = [
-      "clock"
+      "hyprland/workspaces"
+      "hyprland/submap"
     ];
 
     modules-center = [
       "hyprland/window"
     ];
 
-    "hyprland/window" = {
-      rewrite = {
-        "(.*) — Zen Browser" = ''${nfIcon "nf-fa-globe"} $1'';
-      };
-    };
+    modules-right = [
+      "mpd"
+      "idle_inhibitor"
+      "pulseaudio"
+      "network"
+      "power-profiles-daemon"
+      "cpu"
+      "memory"
+      "temperature"
+      "backlight"
+      "keyboard-state"
+      "hyprland/language"
+      "battery"
+      "battery#bat2"
+      "clock"
+      "tray"
+    ];
+
+    # modules-left = [
+    #   "clock"
+    # ];
+
+    # modules-center = [
+    #   "hyprland/window"
+    # ];
+
+    # "hyprland/window" = {
+    #   rewrite = {
+    #     "(.*) — Zen Browser" = ''${nfIcon "nf-fa-globe"} $1'';
+    #   };
+    # };
   };
 }
