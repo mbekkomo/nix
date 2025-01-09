05c8dba72819ba98bd240c5b991efe10ee8b5e7c
Komo <afiqquraisyzulkarnain@gmail.com>
2025-01-10 04:48:41 +0800

    changes from nixos on Fri Jan 10 04:48:41 AM WITA 2025

--- Aspire-TC-605/Bakefile.sh
+++
@@ -1,7 +0,0 @@
-task.switch() {
-  nixos-rebuild switch --flake .#goat --impure "$@"
-}
-
-task.boot() {
-  nixos-rebuild boot --flake .#goat --impure "$@"
-}
--- Aspire-TC-605/README.md
+++
@@ -1,7 +0,0 @@
-Configuration for HP 240 G5 Notebook
-
-To use, run
-```sh
-./bake switch
-```
-Optionally run with `sudo`.
