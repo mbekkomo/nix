c55e8980988f8da76f561ba5dcc9eb74c95055d4
Komo <afiqquraisyzulkarnain@gmail.com>
2025-01-12 15:08:13 +0800

    changes from nixos on Sun Jan 12 03:08:13 PM WITA 2025

--- dotfiles/home.nix
+++ dotfiles/home.nix
@@ -235,12 +235,14 @@ in
 
       set -Ux fifc_editor hx
 
-      fish_vi_key_bindings
       set fish_cursor_default block
       set fish_cursor_insert line
       set fish_cursor_replace_one underscore
       set fish_cursor_replace underscore
       set fish_cursor_visual block
+
+      fish_default_key_bindings -M insert
+      fish_vi_key_bindings --no-erase insert
     '';
     plugins =
       let
