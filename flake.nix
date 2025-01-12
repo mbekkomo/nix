00c447bbd0d3a733d99e6ff1523029d80de6e28c
Komo <afiqquraisyzulkarnain@gmail.com>
2025-01-12 08:36:53 +0800

    changes from nixos on Sun Jan 12 08:36:53 AM WITA 2025

--- flake.nix
+++ flake.nix
@@ -33,6 +33,15 @@
     };
   };
 
+  nixConfig = {
+    extra-subtituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   outputs =
     {
       nixpkgs,
